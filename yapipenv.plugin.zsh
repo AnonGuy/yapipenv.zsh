9daa9a489bd25c3bf95d21d6700447d441c85193
Jeremiah Boby <mail@jeremiahboby.me>
2019-01-28 19:03:07 +0000

    Added support for sub-directory of Pipfile

--- yapipenv.plugin.zsh
+++ yapipenv.plugin.zsh
@@ -1,14 +1,14 @@
 yapipenv-check() {
-
+  
   if [[ $PWD/ != $PIPFILE_PATH/* ]] && [ "$PIPFILE_PATH" ]; then
     # Exit if you are outside the pipenv project directory
-    export PIPFILE_PATH=""
     deactivate
   fi
 
-  if [ -e "$PWD/Pipfile" ] && [ ! "$PIPENV_ACTIVE" ]; then
+  export PIPFILE_PATH="$(pipenv --where 2>/dev/null)"
+
+  if [ "$PIPFILE_PATH" ] && [ ! "$PIPENV_ACTIVE" ]; then
     # Pipenv environment has not been activated
-    export PIPFILE_PATH="$PWD"
     . $(pipenv --venv)/bin/activate
   fi
 
