f4622d34d05904c29d6cd34f970c846e5af39931
Jeremiah Boby <mail@jeremiahboby.me>
2019-01-28 18:39:30 +0000

    Git rekt

---
+++ README.md
@@ -0,0 +1,1 @@
+# yapipenv.zsh
---
+++ yapipenv.plugin.zsh
@@ -0,0 +1,20 @@
+yapipenv-check() {
+
+  if [ -e "$PWD/Pipfile" ] && [ ! "$PIPENV_ACTIVE" ]; then
+    # Pipenv environment has not been activated
+    export PIPFILE_PATH="$PWD"
+    . $(pipenv --venv)/bin/activate
+  fi
+
+  if [[ $PWD/ != $PIPFILE_PATH/* ]] && [ "$PIPFILE_PATH" ]; then
+    # Exit if you are outside the pipenv project directory
+    export PIPFILE_PATH=""
+    deactivate
+  fi
+
+}
+
+chpwd_functions+=(yapipenv-check)
+
+# Enable pipenv tab completions
+eval "$(pipenv --completion)"
