0ba176b14fce0c40c08ccf4d0ff3154d5e6687d3
Jeremiah Boby <mail@jeremiahboby.me>
2019-01-28 18:50:44 +0000

    Added functionality for switching between Pipfiles

--- yapipenv.plugin.zsh
+++ yapipenv.plugin.zsh
@@ -1,15 +1,15 @@
 yapipenv-check() {
 
-  if [ -e "$PWD/Pipfile" ] && [ ! "$PIPENV_ACTIVE" ]; then
-    # Pipenv environment has not been activated
-    export PIPFILE_PATH="$PWD"
-    . $(pipenv --venv)/bin/activate
-  fi
-
   if [[ $PWD/ != $PIPFILE_PATH/* ]] && [ "$PIPFILE_PATH" ]; then
     # Exit if you are outside the pipenv project directory
     export PIPFILE_PATH=""
     deactivate
+  fi
+
+  if [ -e "$PWD/Pipfile" ] && [ ! "$PIPENV_ACTIVE" ]; then
+    # Pipenv environment has not been activated
+    export PIPFILE_PATH="$PWD"
+    . $(pipenv --venv)/bin/activate
   fi
 
 }
