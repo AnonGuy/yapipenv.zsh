667c72b0088f110210152a4bdc77b24965b84361
Jeremiah Boby <mail@jeremiahboby.me>
2019-01-28 19:25:37 +0000

    Added early returns and prioritized function

--- yapipenv.plugin.zsh
+++ yapipenv.plugin.zsh
@@ -3,13 +3,17 @@ yapipenv-check() {
   if [[ $PWD/ != $PIPFILE_PATH/* ]] && [ "$PIPFILE_PATH" ]; then
     # Exit if you are outside the pipenv project directory
     deactivate
+    return
   fi
 
-  export PIPFILE_PATH="$(pipenv --where 2>/dev/null)"
+  if [ ! "$PIPENV_ACTIVE" ]; then
+    PIPFILE_PATH="$(pipenv --where 2>/dev/null)"
 
-  if [ "$PIPFILE_PATH" ] && [ ! "$PIPENV_ACTIVE" ]; then
-    # Pipenv environment has not been activated
-    . $(pipenv --venv)/bin/activate
+    if [ "$PIPFILE_PATH" ]; then
+      # Pipenv environment has not been activated
+      . $(pipenv --venv)/bin/activate
+      export PIPFILE_PATH
+    fi
   fi
 
 }
